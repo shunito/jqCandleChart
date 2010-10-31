e6449b3073ac870316212604153a04dba8a005b3
伊藤 俊輔 <shunsuke.ito@gmail.com>
2010-10-31 21:36:36 +0900

    テストケース追加：デフォルトオプションの上書き

--- test/test-candlechart.js
+++ test/test-candlechart.js
@@ -26,7 +26,7 @@ test("Options", function () {
       'voColor': "#E0FFFF",
       'liColor': "#008080",
       'maColor': "#FFE",
-      'liNum': 5,
+      'liNum': 6,
       'upper' : 15000,
       'lower' : 7000,
       'autoScale' : true
@@ -38,7 +38,17 @@ test("Options", function () {
   equals(stat.options.height ,400, "Canvasの幅を400に設定");
   equals(stat.options.ofX ,40, "Canvas内の横の余白を40に設定");
   equals(stat.options.ofY ,30, "Canvas内の縦の余白を30に設定");
-
-  
+  equals(stat.options.bgColor ,"#2F4F4F", "背景色を#2F4F4Fに設定");
+  equals(stat.options.cdWidth ,5, "ローソクの幅を5に設定");
+  equals(stat.options.cdLineColor ,"#FFF", "ローソクの枠線色を#FFFに設定");
+  equals(stat.options.cdUpColor ,"#FF4500", "値上色を#FF4500に設定");
+  equals(stat.options.cdDownColor ,"#00CED1", "値上色を#00CED1に設定");
+  equals(stat.options.voColor ,"#E0FFFF", "出来高色を#E0FFFFに設定");
+  equals(stat.options.liColor ,"#008080", "罫線色を#008080に設定");
+  equals(stat.options.maColor ,"#FFE", "移動平均線の色を#FFEに設定");
+  equals(stat.options.liNum ,6 , "横罫線数を6に設定");
+  equals(stat.options.upper ,15000 , "表の上限値を15000に設定");
+  equals(stat.options.lower ,7000 , "表の下限値を7000に設定");
+  equals(stat.options.autoScale ,true , "表の上限と下限値を自動設定に設定");
 });
 
